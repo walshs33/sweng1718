a7e426a99d4177fa1121e5c4eaefddda05f95811
walshs33 <walshs33@tcd.ie>
2017-10-17 14:29:50 +0100

    initial setup of project

---
+++ sweng_assignment_1/src/sweng_assignment_1/Sweng_assignment_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sweng_assignment_1;
+
+/**
+ *
+ * @author Sam Walsh - 12304297
+ */
+public class Sweng_assignment_1
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
---
+++ sweng_assignment_1/test/ass_1_test.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sam Walsh - 12304297
+ */
+public class ass_1_test
+{
+    
+    public ass_1_test()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+    
+}
