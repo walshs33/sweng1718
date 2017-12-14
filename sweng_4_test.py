c0697b08eeed520ab6e5a0c0ca211be1a7f50f89
walshs33 <walshs33@tcd.ie>
2017-12-14 13:43:32 +0000

    modified assignment 3 to work in ipython notebook

---
+++ sweng_4_test.py
@@ -0,0 +1,15 @@
+from github import Github
+import getpass
+
+#user input
+username = "walshs33"
+pword = getpass.getpass("password: ")
+
+g = Github(username, pword)
+
+print("repos:")
+for repo in g.get_user().get_repos():
+        print(repo.name)
+
+print("no. of private repos:")
+print g.get_user().total_private_repos
